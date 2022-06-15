4d9f721746a5a590bb281b92322958fa27e645ed
abimont <abimontes00@gmail.com>
2022-06-14 22:15:48 -0700

    print lowercase alphabet using putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet
+ * Return: 0
+ */
+
+int main(void)
+
+{
+int alphabet = 96;
+while (alphabet < 122)
+{
+((alphabet++));
+putchar(alphabet);
+putchar('\n');
+}
+
+return (0);
+}
