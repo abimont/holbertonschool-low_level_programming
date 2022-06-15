a4e114464b41841911620cacc2913612ea196b6a
abimont <abimontes00@gmail.com>
2022-06-15 16:41:02 -0700

    numbers in base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print single digit
+ *Return: 0
+ */
+
+int main(void)
+
+{
+int n = 0;
+while (n <= 9)
+{
+printf("%d", n);
+((n++));
+}
+printf("\n");
+return (0);
+}
