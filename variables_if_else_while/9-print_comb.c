8f3b69df0c778b97458f00cd23000845cc1761f5
abimont <abimontes00@gmail.com>
2022-06-15 20:45:02 -0700

    combinations

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,13 +8,21 @@ int main(void)
 
 {
 int num = 47;
+int com = 44;
+int spc = 32;
 
 while (num < 57)
 {
 ((num++));
 putchar(num);
-putchar(44);
-putchar(32);
+if (num < 57)
+{
+putchar(com);
+}
+if (num < 57)
+{
+putchar(spc);
+}
 }
 putchar('\n');
 return (0);
