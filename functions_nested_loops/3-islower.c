bac62d7bf21f8e29828240aa5c71c1e4febea586
abimont <abimontes00@gmail.com>
2022-06-17 14:40:15 -0700

    checks for lowercase characters

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_islower - checks for lowercase character
+ *Return: 0
+ *@c: variable
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
