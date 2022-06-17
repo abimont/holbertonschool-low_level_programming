9fb6831c08a4506f3d74c29fc1b7bd4f75c02a87
abimont <abimontes00@gmail.com>
2022-06-17 16:53:53 -0700

    prints the sing of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_sign - prints the sign of a number
+ *Return: 0
+ *@n: variable
+ */
+
+int print_sign(int n)
+{
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+
+}
