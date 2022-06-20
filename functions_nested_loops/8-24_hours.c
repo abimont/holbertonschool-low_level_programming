a88dfab450ab3ee31e8a53b4fdee8f34ff3347f0
abimont <abimontes00@gmail.com>
2022-06-20 11:16:41 -0700

    Jack Bauer - 24 hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints 24 hours
+ * Return:0
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+
+		}
+	}
+}
