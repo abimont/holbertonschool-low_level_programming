28da98be548d74503f3247b08db5c9fba683f958
abimont <abimontes00@gmail.com>
2022-06-20 12:10:46 -0700

    adds two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - adds two integers
+ *Return: sum
+ *@var1: variable
+ *@var2: variable
+ */
+
+int add(int var1, int var2)
+{
+	int sum = var1 + var2;
+
+	return (sum);
+}
