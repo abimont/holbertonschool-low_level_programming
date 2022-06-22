90c69d15da7625aa03ac35d923db5efb95cd8c71
abimont <abimontes00@gmail.com>
2022-06-21 20:05:48 -0700

    draws a diagonal line

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,11 +9,17 @@
 void print_diagonal(int n)
 {
 	int bslash = 92;
+	int space;
 	int t;
 
 	for (t = 1; t <= n; t++)
 	{
+		for (space = 0; space <= t; space++)
+		{
+			_putchar(' ');
+		}
 		_putchar(bslash);
 		_putchar('\n');
+
 	}
 }
