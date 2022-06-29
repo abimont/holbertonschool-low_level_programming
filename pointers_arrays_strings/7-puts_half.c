31ccc979c67aaeff5b37919b4919f6b361d538d1
abimont <abimontes00@gmail.com>
2022-06-29 00:10:56 -0700

    prints half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ *_strlen - length of a string
+ *@str: text string
+ *Return: len
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * puts_half - prints a half of a string
+ * @str: text string
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int half = len / 2;
+	int middle = len % 2;
+	int odd = (len - 1) / 2;
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		while (str[half] != '\0' && str[odd] != '\0')
+		{
+			if (middle == 0)
+			{
+				_putchar(str[half]);
+				half++;
+			}
+
+			if (middle != 0)
+			{
+				_putchar(str[odd]);
+				odd++;
+			}
+
+		}
+		_putchar('\n');
+	}
+
+}
