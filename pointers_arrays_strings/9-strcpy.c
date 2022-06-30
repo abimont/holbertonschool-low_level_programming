825cbe1bb56f1afe09182756733036b78c7ffeba
abimont <abimontes00@gmail.com>
2022-06-29 21:19:35 -0700

    copies a string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
+	int i = 0;
 
-	while (*src != '\0')
+	while (*(src + i))
 	{
-		*dest = *src;
-
-		src++;
-		dest++;
+		*(dest + i) = *(src + i);
+		i++;
 	}
+
+
 	return (dest);
 }
