0077bf535b80fe0783ac902c9aba063526ad3c4a
abimont <abimontes00@gmail.com>
2022-07-10 18:52:59 -0700

    allocates memory using malloc

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
 /**
  * *malloc_checked - allocates memory using malloc
  * @b: size
- * Return: pointer to the allocated memory
+ * Return: pointer to allocated memory
  */
 
 void *malloc_checked(unsigned int b)
