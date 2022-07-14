5248a5dca6d351cdc4f5ef1ebdaadb069b73f0af
abimont <abimontes00@gmail.com>
2022-07-13 18:18:33 -0700

    define a new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - Short description
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
