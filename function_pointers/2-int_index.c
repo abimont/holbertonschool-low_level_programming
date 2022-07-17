435b9a1f2cd686e3d55abe82717b77e40f28a6cc
abimont <abimontes00@gmail.com>
2022-07-16 17:09:21 -0700

    searches for an integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,12 +17,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0 || !array)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	if (cmp && array)
 	{
-		if (cmp(array[i]) != 0)
-			break;
-		if (i == size - 1)
-			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				break;
+			if (i == size - 1)
+				return (-1);
+		}
 	}
 
 	return (i);
