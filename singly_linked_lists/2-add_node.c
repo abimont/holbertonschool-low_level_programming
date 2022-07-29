fc0838b550775225faa75cd1571b899467e43049
abimont <abimontes00@gmail.com>
2022-07-29 13:09:22 -0700

    len fixed

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,14 +11,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int new_len;
 
+	new_len = 0;
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	while (str[new_len])
+		new_len++;
+
 	new_node->str = strdup(str);
-	new_node->next = (*head);
-	(*head) = new_node;
+	new_node->next = *head;
+	new_node->len = new_len;
+	*head = new_node;
 
 	return (new_node);
 }
