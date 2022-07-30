c968c846dde73d95768b9b11f896c9245a9ecf26
abimont <abimontes00@gmail.com>
2022-07-29 20:28:52 -0700

    deletes the head node of a list

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a list
+ * @head: first node of the list
+ *
+ * Return: head node’s data (n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	int ndata;
+	listint_t *temp;
+
+	temp = *head;
+	ndata = temp->n;
+	*head = temp->next;
+	free(temp);
+
+	return (ndata);
+}
