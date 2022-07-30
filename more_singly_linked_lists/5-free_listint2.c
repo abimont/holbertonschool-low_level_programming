bf2f5cd027a0cb703cdb2a43e6929ace435ef800
abimont <abimontes00@gmail.com>
2022-07-29 19:06:32 -0700

    frees a list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: first node of the list
+ *
+ * Return:  void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = NULL;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = temp->next;
+		if (temp->next == NULL)
+			*head = NULL;
+		free(temp);
+	}
+}
