5848e11d41c454741a87985a308985855cc0959c
abimont <abimontes00@gmail.com>
2022-07-29 21:12:28 -0700

    returns the nth node

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a list
+ * @head: first node of the list
+ * @index: number of position of the node to return
+ *
+ * Return: nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int iterator;
+	listint_t *tmp;
+
+	iterator = 0;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		if (index == iterator)
+			break;
+		iterator++;
+	}
+
+	if (index > iterator)
+		return (NULL);
+
+	return (tmp);
+}
