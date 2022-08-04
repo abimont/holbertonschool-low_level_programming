20b7048b9be0a0bbeaff1ad9fcc851636ca0e482
abimont <abimontes00@gmail.com>
2022-08-03 20:49:30 -0700

    read and print to stdout

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: text file to read 
+ * @filename: text file to read
  * @letters: number of letters it should read and print
  *
  * Return: actual number of letters it could read and print
