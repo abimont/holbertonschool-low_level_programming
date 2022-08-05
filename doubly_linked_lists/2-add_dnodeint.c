95e1a606793df0f5328a55645721bf4f75fae77c
abimont <abimontes00@gmail.com>
2022-08-05 15:26:12 -0700

    adds a new node at the beginning

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	if (*head == NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+
+		return (new_node);
+	}
 	new_node->n = n;
 	new_node->next = *head;
 	new_node->prev = NULL;
