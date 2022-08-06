400c799d0dba0c6538a3b32e5190ba0360e0c73d
abimont <abimontes00@gmail.com>
2022-08-05 22:32:55 -0700

    deletes the node at index

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,9 +23,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = temp_node->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free (temp_node);
+		if (temp_node->next != NULL)
+			temp_node->next->prev = NULL;
+		free(temp_node);
 		return (1);
 	}
 
@@ -40,7 +40,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	prev_node->next = temp_node->next;
-	temp_node->next->prev = temp_node->prev;
+	temp_node->next->prev = prev_node;
 	free(temp_node);
 
 	return (1);
