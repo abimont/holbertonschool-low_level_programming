f7b65ee6b3b118daafbfd892d571e2ce438dbc63
abimont <abimontes00@gmail.com>
2022-08-30 19:34:15 -0700

    case fixed

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_element = NULL;
 	unsigned long int index = 0;
 
-	if (!key)
+	if (!key || !ht)
 		return (0);
 
 	new_element = malloc(sizeof(hash_node_t));
