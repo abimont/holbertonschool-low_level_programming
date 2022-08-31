6440731f19a7aebb120f5ab91948c4d46f67996c
abimont <abimontes00@gmail.com>
2022-08-30 20:18:38 -0700

    retrieves a value associated with a key

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key to looking for
+ *
+ * Return: value associated with element or NULL if key is not found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *list;
+	unsigned long int index = key_index((unsigned const char *) key, ht->size);
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	for (list = ht->array[index]; list != NULL; list = list->next)
+	{
+		if (strcmp(key, list->key) == 0)
+			return (list->value);
+	}
+
+	return (NULL);
+}
