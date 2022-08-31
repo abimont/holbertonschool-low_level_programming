3623485a36317b1e7cdeb9652c8444cd75971858
abimont <abimontes00@gmail.com>
2022-08-31 13:18:52 -0700

    prints a hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *node;
+	int n = 0;
+
+	printf("{");
+
+	for (index = 0; ht && index < ht->size; index++)
+	{
+		node = ht->array[index];
+		while (node)
+		{
+			if (node->key != NULL && n == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			n = 1;
+
+		}
+	}
+	printf("}");
+	printf("\n");
+}
